5ebb4c8bf1dddf1094b8796681daccae29ec7555
Stephen <saintobis11@gmail.com>
2023-06-06 15:10:04 +0100

    signup event handler templates

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
+import useForm from "../../utils/useForm";
 
 const SignUp = () => {
+
+    const onEnter = (e) => {
+        e.stopPropagation();
+        if (e.key === "Enter") {
+            signUpHandler();
+        }
+      };
+    
+      const signUpHandler = () => {
+        console.log(inputValues);
+      };
+    
+    const [inputValues, onChangeHandler, onSubmitHandler] = useForm(signUpHandler);
+
     return ( 
         <div className="signup-screen">
             <div className="left">
@@ -21,33 +36,71 @@ const SignUp = () => {
                     <form action="">
                     <div className="pos-rel">
                         <label className="abs py-10"> Store Name*</label>
-                        <input type="text" name="store-name" className="form-control"/>
+                        <input
+                         type="text" 
+                         name="storeName" 
+                         className="form-control" 
+                         onChange={onChangeHandler}
+                         value={inputValues.storeName || ""}
+                    />
                     </div>
                         <span className="flex">
                             <div className="pos-rel">
                                 <label className="abs py-10"> Email*</label>
-                                <input type="email" name="email" className="email form-control"/>
+                                <input
+                                 type="email" 
+                                 name="email" 
+                                 className="email form-control" 
+                                 onChange={onChangeHandler}
+                                 value={inputValues.email || ""}
+                                />
                             </div>
                             <div className="pos-rel">
                                 <label className="abs py-10"> Phone Number*</label>
-                                <input type="text" name="phone-number" className="phone form-control"/>
+                                <input
+                                 type="text" 
+                                 name="phoneNumber" 
+                                 className="phone form-control"
+                                 onChange={onChangeHandler}
+                                 value={inputValues.phoneNumber || ""}
+                                />
                             </div>
                         </span>
                         <span className="flex">
                             <div className="pos-rel">
                                 <label className="abs py-10"> First Name* </label>
-                                <input type="text" name="first-name"  className="first-name form-control"/>
+                                <input
+                                 type="text" 
+                                 name="firstName" 
+                                 className="first-name form-control"
+                                 onChange={onChangeHandler}
+                                 value={inputValues.firstName || ""}
+                                />
                             </div>
                             <div className="pos-rel">
                                 <label className="abs py-10"> Last Name* </label>
-                                <input type="text" name="last-name"  className="last-name form-control"/>
+                                <input
+                                 type="text" 
+                                 name="lastName" 
+                                 className="last-name form-control"
+                                 onChange={onChangeHandler}
+                                 value={inputValues.lastName || ""}
+                                />
                             </div>
                         </span>
                         <span className="t-and-c">
-                            <input type="checkbox" name="t-and-c" />
+                            <input
+                             type="checkbox" 
+                             name="t-and-c" 
+                            />
                             <p>I agree to the <Link to="/app/Terms" className="purple">terms</Link> and <Link to="/app/Conditions" className="purple">conditions</Link> of Pmall Nigeria</p>
                         </span>
-                        <button className="continue-btn">Continue</button>
+                        <button
+                         className="continue-btn" 
+                         onClick={signUpHandler}
+                        >
+                            Continue
+                        </button>
                         <p className="have-an-account">Already have an account?</p>
                         <Link to="/app/Login"><button className="back-to-login">Back to Login</button></Link>
                     </form>
