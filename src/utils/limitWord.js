f81d82a70c2a04730164b684b61630229ee13975
Ahmed Peter <talk2ahmedpeter@gmail.com>
2024-10-24 20:14:35 +0100

    Resolved failed build

---
+++ src/utils/limitWord.js
@@ -0,0 +1,6 @@
+const LimitWord = (text, length) => {
+    const limitedText = text.split(' ').slice(0, length).join(' ') + (text.split(' ').length > length ? '...' : '');
+    return limitedText;
+  };
+
+  export default LimitWord;
---
+++ src/utils/loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import MoonLoader from "react-spinners/MoonLoader";
+
+
+export default function Loading({ loading }) {
+  return (
+    <div className={`loading ${loading ? "active" : null}`}>
+
+      <MoonLoader
+        color="#1DCF9F"
+        size={35}
+        speedMultiplier={1}
+        loading={loading}
+      />
+{/* 
+
+
+      <MoonLoader
+        color='#1DCF9F'
+        loading={loading}
+        size={35}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      /> */}
+    </div>
+  );
+}
