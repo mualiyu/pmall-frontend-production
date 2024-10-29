688f9f485c3768e169d8fb7118e0592f55cbcb92
Ahmed Peter <talk2ahmedpeter@gmail.com>
2024-10-29 19:41:01 +0100

    updated outdated browserlist package

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import "./assets/fonts/fonts/fonts.css";
 import Application from "./components";
-// import { UserProvider } from "../context/UserContext";
 import { UserProvider } from "./context/UserContext";
 import { VendorSignupProvider } from "./context/VendorSignupContext";
 import { CartProvider } from "./context/CartContext";
