84f4a007e2eab74da6607b5a21b7da34fd8733ff
Stephen <saintobis11@gmail.com>
2025-04-14 19:33:02 +0100

    sidebar-done

--- src/components/builder/SidebarRow.js
+++ src/components/builder/SidebarRow.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useVendor } from "../../context/VendorSignupContext";
 import "./Builder.css";
 
 function SidebarRow({ Icon, title, path, onClick }) {
@@ -8,8 +9,14 @@ function SidebarRow({ Icon, title, path, onClick }) {
       onClick();
     }
   };
+  const {
+    setVisible,
+  } = useVendor();
+  const handleLinkClick = () => {
+    setVisible(false);
+  };
   return (
-    <Link to={path} style={{ textDecoration: "none" }}>
+    <Link to={path} style={{ textDecoration: "none" }} onClick={handleLinkClick}>
       <div className="sidebarRow alc" onClick={handleClick}>
         {Icon && <Icon className="sidebar__icon icon___light icon___normal" />}
         <p className="sidebar__title"> {title} </p>
